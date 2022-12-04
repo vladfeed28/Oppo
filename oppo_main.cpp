edbd9cd71abefa25d0c5aaf8553e03d1e868d2d4
vladfeed28 <62261137+vladfeed28@users.noreply.github.com>
2022-12-04 19:52:12 +0700

    bug_fix

--- oppo_main.cpp
+++ oppo_main.cpp
@@ -1,9 +1,18 @@
 ﻿#include <iostream>
 
-double my_pow(double x, unsigned int y) {
+double my_pow(double x, int y) {
+	bool buff = false;	
+	if (y<0) {
+		y = -y;
+		buff = true;
+	}
+	
 	double count = 1;
 	for (int i = 0; i < y; i++) {
 		count *= x;
 	}
+	
+	if (buff) count = 1 / count;
 	return count;
 }
+
